73849cb00611b03f8d8eb6161f9f138111c4a58b
indream <flashindream@gmail.com>
2016-05-09 15:55:02 +0800

    update

---
+++ app.js
@@ -0,0 +1,25 @@
+var twophase = require('./lib/index');
+
+console.log("Initiating algorithm...");
+twophase.initialize(function () {
+	console.log("Ready");
+
+	// This just tests the solve, solution is F
+	twophase.solve("UUUUUURRRDRRDRRDRRFFFFFFFFFLLLDDDDDDLLULLULLUBBBBBBBBB", 30, 60, false, function (err, solution) {
+		if (err) console.error(err);
+
+		twophase.randomCube(function (err, randomCube) {
+			if (err) console.error(err);
+			var cube = randomCube;
+			//cube = "DUDLUDLFDRBRFRBLUUBUBLFRLDFFFUDDBRDRBFUBLUULDFRLRBLBRF";
+			console.log(cube);
+
+			twophase.solve(cube, 30, 60, false, function (err, solution) {
+				if (err) console.error(err);
+				console.log("Solution:", solution);
+				twophase.close();
+			});
+		});
+		
+	});
+});
--- server.js
+++ server.js
@@ -1,34 +1,81 @@
-var express = require('express')
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var cam = require("linuxcam");
-var jpeg = require('jpeg-js');
+var childProcess = require('child_process')
+  , express = require('express')
+  , http = require('http')
+  , morgan = require('morgan')
+  , ws = require('ws');
 
-cam.start('/dev/video0',320,240);
-function update(socket) {
-  var frame = cam.frame();
-  var rawImageData = {
-  data: frame.data,
-  width: frame.width,
-  height: frame.height
-};
-  var jpegFrame = jpeg.encode(rawImageData,80);
-console.log(frame.data.length,jpegFrame.data.length);
-  socket.emit("frame", jpegFrame.data.toString('base64'));
-  setTimeout(function() {
-    update(socket);
-  }, 40);
+// configuration files
+var configServer = require('./lib/config/server');
 
-}
- 
-io.on('connection', function(socket){
-  socket.on('error', function(err){
-    console.log("ERROR: "+err);
+console.log("READY");
+
+var app = express();
+app.set('port', configServer.httpPort);
+app.use(express.static(configServer.staticFolder));
+app.use(morgan('dev'));
+
+
+// serve index
+require('./lib/routes').serveIndex(app, configServer.staticFolder);
+
+// HTTP server
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('HTTP server listening on port ' + app.get('port'));
+});
+var STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
+var width = 640;
+var height = 480;
+
+// WebSocket server
+var wsServer = new (ws.Server)({ port: configServer.wsPort });
+console.log('WebSocket server listening on port ' + configServer.wsPort);
+
+wsServer.on('connection', function(socket) {
+  // Send magic bytes and video size to the newly connected socket
+  // struct { char magic[4]; unsigned short width, height;}
+  var streamHeader = new Buffer(8);
+
+  streamHeader.write(STREAM_MAGIC_BYTES);
+  streamHeader.writeUInt16BE(width, 4);
+  streamHeader.writeUInt16BE(height, 6);
+  socket.send(streamHeader, { binary: true });
+
+  console.log('New WebSocket Connection (' + wsServer.clients.length + ' total)');
+
+  socket.on('close', function(code, message){
+    console.log('Disconnected WebSocket (' + wsServer.clients.length + ' total)');
   });
-  update(socket);
 });
- app.use(express.static('public'));
-http.listen(9639, function(){
-  console.log('listening on *:9639');
+
+wsServer.broadcast = function(data, opts) {
+  for(var i in this.clients) {
+    if(this.clients[i].readyState == 1) {
+      this.clients[i].send(data, opts);
+    }
+    else {
+      console.log('Error: Client (' + i + ') not connected.');
+    }
+  }
+};
+function resetStream(){
+  childProcess.exec('./bin/do_ffmpeg.sh');
+};
+// HTTP server to accept incoming MPEG1 stream
+var server = http.createServer(function (req, res) {
+  console.log(
+    'Stream Connected: ' + req.socket.remoteAddress +
+    ':' + req.socket.remotePort + ' size: ' + width + 'x' + height
+  );
+
+  req.on('data', function (data) {
+    wsServer.broadcast(data, { binary: true });
+  });
 });
+server.listen(configServer.streamPort, function () {
+  console.log('Listening for video stream on port ' + configServer.streamPort);
+
+  // Run do_ffmpeg.sh from node                                                   
+  childProcess.exec('./bin/do_ffmpeg.sh');
+});
+server.timeout = 0;
+module.exports.app = app;
